cfc49163a7db0f87bd42dfcb760c03ad86820469
princenyamadi <princenyamadi.pn@gmail.com>
2020-11-30 07:29:50 +0000

    Played around with a timer and a countdown timer

--- app/src/main/java/tech/peny/timerdemo/MainActivity.java
+++ app/src/main/java/tech/peny/timerdemo/MainActivity.java
@@ -3,6 +3,9 @@ package tech.peny.timerdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new CountDownTimer(10000,1000){
+            public void onTick(long millisecondsUntilDone){
+                      Log.i("Seconds Left", String.valueOf(millisecondsUntilDone / 1000) );
+            }
+            public void onFinish(){
+                Log.i("We're Done!!!", "No More Count Down");
+            }
+        }.start();
+
+
+//        final Handler handler = new Handler();
+//
+//        Runnable run  = new Runnable() {
+//            @Override
+//            public void run() {
+//                Log.i("Hey it's us","a second has passed");
+//                handler.postDelayed(this,1000);
+//            }
+//        };
+//        handler.post(run);
     }
 }
